34a1b05c42c9860fe091310d32b6d0b0674a9181
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-04 03:33:48 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,10 @@
 # Write your solution here
-run_code_inside = FALSE
-puts "Code before if...end"
-if run_code_inside
-  puts "Code after if...end"
-end
+#run_code_inside = FALSE
+###Eend
+if true 
+  puts "Even!"
+else
+  puts "Odd!"
+end 
+current_time = Time.authenticate_or_request_with_http_digest
+current_time.to_i
