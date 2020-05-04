4ee452191032d81b20f5ccd9bfd6739bb4d50380
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-04 03:35:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,10 @@
 # Write your solution here
 #run_code_inside = FALSE
 ###Eend
-if true 
+if current_time % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
 end 
 current_time = Time.authenticate_or_request_with_http_digest
-current_time.to_i
+current_time = current_time.to_i
